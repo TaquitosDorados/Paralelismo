64fdd5d91b30cf96af2d0238214cd2a0628f4366
TaquitosDorados <62762581+TaquitosDorados@users.noreply.github.com>
2023-10-10 13:48:43 -0600

    Three Channels on Gray

---
+++ Actividad 2/image/cmake-build-debug-visual-studio/CMakeFiles/image.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image.dir/main.cpp.obj"
+  "CMakeFiles/image.dir/main.cpp.obj.d"
+  "image.exe"
+  "image.exe.manifest"
+  "image.lib"
+  "image.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Actividad 2/image/main.cpp
+++ Actividad 2/image/main.cpp
@@ -1,8 +1,55 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
+#include <filesystem>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string image_path = "C:\\Users\\xxdar\\Pictures\\mrbeast.jpg";
+    std::cout << "Enter the path to the image" << std::endl;
+    std::cin >> image_path;
+
+    if(!std::filesystem::exists(image_path)){
+        std::cout << "File does not exist at the specified path" << std::endl;
+        return -1;
+    }
+
+    cv::Mat image = cv::imread(image_path);
+
+    if(image.empty()){
+        std::cout << "Error loading the image" << std::endl;
+        return -1;
+    }
+    else {
+        std::cout << "Image loaded successfully" << std::endl;
+    }
+
+    cv::imshow("Image", image);
+
+    cv::waitKey(0);
+
+    cv::Mat bgr[3];
+    cv::split(image, bgr);
+
+    cv::Mat blueChannel, greenChannel, redChannel;
+
+    cv::applyColorMap(bgr[0], blueChannel, cv::COLORMAP_JET);
+    cv::applyColorMap(bgr[1], greenChannel, cv::COLORMAP_SPRING);
+    cv::applyColorMap(bgr[2], redChannel, cv::COLORMAP_HOT);
+
+    cv::imshow("Blue Channel", blueChannel);
+    cv::imshow("Green Channel", greenChannel);
+    cv::imshow("Red Channel", redChannel);
+
+    cv::waitKey(0);
+
+    cv::applyColorMap(bgr[0], blueChannel, cv::COLORMAP_BONE);
+    cv::applyColorMap(bgr[1], greenChannel, cv::COLORMAP_BONE);
+    cv::applyColorMap(bgr[2], redChannel, cv::COLORMAP_BONE);
+
+    cv::imshow("Blue Channel", blueChannel);
+    cv::imshow("Green Channel", greenChannel);
+    cv::imshow("Red Channel", redChannel);
+
+    cv::waitKey(0);
     return 0;
 }
